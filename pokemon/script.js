0764494e4e269405f66473f330a059cf2aa69618
Yisus <yisusmurci@gmail.com>
2024-02-16 17:35:47 -0500

    Añadidas imagen y hp

--- pokemon/script.js
+++ pokemon/script.js
@@ -3,23 +3,25 @@ const searchBtn= document.getElementById("search-button");
 
 const pokemonName= document.getElementById("pokemon-name");
 const pokemonId= document.getElementById("pokemon-id");
+const pokemonHp= document.getElementById("hp");
 const pokemonWeight= document.getElementById("weight");
 const pokemonHeight= document.getElementById("height");
 const types= document.getElementById("types");
 const pokemonSpecialAtk= document.getElementById("special-attack");
 const pokemonSpecialDfe= document.getElementById("special-defense");
 const pokemonSpeed= document.getElementById("speed");
+const divEl= document.querySelector(".stats");
 
 const showData= (data)=>{
-    const {name, id, weight, height, type, specialAtk, specialDfe, speed}= data;
+    const {name, id, weight, height, order, stats, sprites}= data;
     pokemonName.innerText= name;
-    pokemonId.innerText= id;
+    pokemonId.innerText= `#${id}`;
+    pokemonHp.innerText= order;
     pokemonWeight.innerText= weight;
     pokemonHeight.innerText= height;
-    types.innerText= type;
-    pokemonSpecialAtk.innerText= specialAtk;
-    pokemonSpecialDfe.innerText= specialDfe;
-    pokemonSpeed.innerText= speed;
+    const img= document.createElement("img");
+    img.src= sprites.front_default;
+    divEl.appendChild(img);
 };
 
 const pokemonApi= async ()=>{
